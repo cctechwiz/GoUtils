e810acf9d14682399581ff283d90bf1bad860a20
Josh Maxwell <cctechwiz@gmail.com>
2019-12-29 23:56:42 -0700

    adding first go util

---
+++ flagHelpers/flagHelpers.go
@@ -0,0 +1,13 @@
+package flagHelpers
+
+import "flag"
+
+func IsFlagPassed(name string) bool {
+	found := false
+	flag.Visit(func(f *flag.Flag) {
+		if f.Name == name {
+			found = true
+		}
+	})
+	return found
+}
